fe3b0703b6689b83ef00fbe4539f58624051d7f6
Bhattacharjee <shbhattacharjee@deloitte.com>
2016-08-24 19:29:36 +0530

    db set up

--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,15 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  someMongodbServer: {
+    adapter: 'sails-mongo',
+    // host: 'localhost',
+    // port: 27017,
+    // user: 'username', //optional
+    // password: 'password', //optional
+    // database: 'your_mongo_db_name_here' //optional
+    host: process.env.MONGOLAB_URI
+  },
 
   /***************************************************************************
   *                                                                          *
